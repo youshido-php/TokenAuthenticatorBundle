cb56c1900067fd72eaf8ea19c3af7cca0b70f18c
Portey Vasil <portey@gmail.com>
2016-02-03 10:03:16 +0200

    add exception listener

---
+++ Service/Listener/ExceptionListener.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Youshido\TokenAuthenticatorBundle\Service\Listener;
+
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
+
+/**
+ * Date: 2/3/16
+ *
+ * @author Portey Vasil <portey@gmail.com>
+ */
+class ExceptionListener
+{
+
+    public function onKernelException(GetResponseForExceptionEvent $event)
+    {
+        $response = new JsonResponse([
+            'errors' => [
+                [
+                    'message' => $event->getException()->getMessage(),
+                    'code'    => $event->getException()->getCode()
+                ]
+            ]
+        ], 200, [
+            'Access-Control-Allow-Origin'  => '*',
+            'Access-Control-Allow-Headers' => 'apikey'
+        ]);
+
+        $event->setResponse($response);
+    }
+
+}
